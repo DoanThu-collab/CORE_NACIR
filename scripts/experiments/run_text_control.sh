f0d98a95838ab2509d8ef674c6184427d0e95d49
Minh Thư <minhthu6801@gmail.com>
2026-08-30 01:36:16 +0700

    Make CLIP download permission explicit in text-control workflow

--- scripts/experiments/run_text_control.sh
+++ scripts/experiments/run_text_control.sh
@@ -19,5 +19,6 @@ PYTHONPATH=src python scripts/experiments/evaluate_text_reencode_control.py \
   --beliefs "$BELIEFS" \
   --adapter-module nacir.adapters.openai_clip_vitl14 \
   --adapter-func load_clip_text_encoder \
+  --allow-download \
   --frozen-h0-ranks runs_final/chatir_clip_vitl14_h0/ranks.npz \
   --output "$OUT_ROOT/clip"
